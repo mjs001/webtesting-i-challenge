35f6a37c7e92bf9b98a1168a3f30e73acf095a1e
miriam-sullivan <maljayd42@gmail.com>
2020-05-20 18:25:24 -0500

    testing my tests

--- enhancing/enhancer.js
+++ enhancing/enhancer.js
@@ -6,15 +6,26 @@ module.exports = {
 };
 
 function succeed(item) {
-  return { ...item };
+  return { ...item }, item.enhancement < 20 && item.enhancement++;
 }
 
 function fail(item) {
-  return { ...item };
+  if (item.enhancement < 15) {
+    item.durability = item.durability - 5;
+    return { ...item };
+  } else if (item.enhancement >= 15) {
+    item.durability = item.durability - 10;
+    if (item.enhancement > 16) {
+      item.enhancement = item.enhancement - 1;
+    }
+    return { ...item };
+  } else {
+    return { ...item };
+  }
 }
 
 function repair(item) {
-  return { ...item };
+  return { ...item }, (item.durability = 100);
 }
 
 function get(item) {
